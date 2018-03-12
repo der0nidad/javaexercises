75ffa90b3c7b408958c74c3a4ca2b8ae188153a2
der0nidad <der0nidad@gmail.com>
2018-03-12 11:07:38 +0300

    Cosmetic comments cleanup

--- src/com/der0nidad/Main.java
+++ src/com/der0nidad/Main.java
@@ -1,12 +1,7 @@
 package com.der0nidad;
 
 
-/*реализовать
-    1. заполнение контейнера данными с клавиатуры
-    2. вывод информации о сотрудниках на экран
-    3. поиск сотрудников, состоящих на данной должности
-    4. сортировку списка сотрудников по полю фамилия и по полю зарплата
-* */
+
 public class Main {
     /*Fills the list of employees with data entered from the keyboard, separated by spaces*/
 
